b10441b55380a1f6d545ac6487ffb28f404d6479
Damián Martínez <p82maavd@uco.es>
2023-01-02 21:43:47 +0100

    commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,31 @@
-
+CREATE DATABASE f1database;
+use f1database;
 
 CREATE TABLE Driver
     (
-     driverId INTEGER NOT NULL PRIMARY KEY,
+     driverId int NOT NULL PRIMARY KEY,
      firstName VARCHAR(30) NOT NULL ,
      lastName VARCHAR(50) NOT NULL ,
      nationality VARCHAR(20) NOT NULL ,
-     birthdate DATE NOT NULL
+     birthdate DATE NOT NULL 
     );
 
 CREATE TABLE Team
     (
-     teamId INTEGER NOT NULL PRIMARY KEY,
+     teamId int NOT NULL PRIMARY KEY,
      name VARCHAR(30) NOT NULL ,
      nationality VARCHAR(20) ,
-     foundation DATE NOT NULL
+     foundation DATE NOT NULL 
     );
 
 
 CREATE TABLE Driver_Team
     (
-     driverId INTEGER NOT NULL FOREIGN KEY REFERENCES Driver(driverId) ,
-     teamId INTEGER NOT NULL FOREIGN KEY REFERENCES Team(teamId),
-     season INTEGER NOT NULL 
+     driverId int NOT NULL ,  
+     teamId int NOT NULL ,
+     season int NOT NULL , 
+     FOREIGN KEY(driverId) REFERENCES Driver(driverId) ,
+     FOREIGN KEY(teamId) REFERENCES Team(teamId),
      CONSTRAINT pk_tbl_Driver_Team  PRIMARY KEY (driverId, teamId, season)
 
     );
@@ -31,10 +34,10 @@ ADD CONSTRAINT CHK_driver_team_season CHECK (season>=1950 AND season<=2022);
 
 CREATE TABLE Circuit
     (
-     circuitId INTEGER NOT NULL PRIMARY KEY ,
+     circuitId int NOT NULL PRIMARY KEY ,
      name VARCHAR(40) NOT NULL ,
      location VARCHAR(30) ,
-     laps INTEGER NOT NULL 
+     laps int NOT NULL 
     );
 
 ALTER TABLE Circuit
@@ -42,11 +45,13 @@ ADD CONSTRAINT CHK_laps CHECK (laps>=1 AND laps<=100);
 
 CREATE TABLE Race
     (
-     raceId INTEGER NOT NULL PRIMARY KEY,
-     circuitId INTEGER NOT NULL FOREIGN KEY REFERENCES Circuit(circuitId),
+     raceId int NOT NULL PRIMARY KEY,
+     circuitId int NOT NULL,
      name VARCHAR(50) NOT NULL ,
-     season INTEGER NOT NULL ,
-     date DATE 
+     season int NOT NULL ,
+     date DATE NOT NULL,
+     FOREIGN KEY(circuitId) REFERENCES Circuit(circuitId)
+     
     );
 
 ALTER TABLE Race
@@ -54,14 +59,16 @@ ADD CONSTRAINT CHK_raceseason CHECK (season>=1950 AND season<=2022);
 
 CREATE TABLE RaceResult
     (
-     driverId INTEGER NOT NULL FOREIGN KEY REFERENCES Driver(driverId),
-     raceId INTEGER NOT NULL FOREIGN KEY REFERENCES Race(raceId),
-     startPosition INTEGER NOT NULL ,
-     position INTEGER NOT NULL ,
+     driverId int NOT NULL,
+     raceId int NOT NULL,
+     startPosition int NOT NULL ,
+     position int NOT NULL ,
      time TIME ,
-     dnf INTEGER NOT NULL ,
-     fastestLap TIME 
-     CONSTRAINT pk_tbl_RaceResult  PRIMARY KEY (driverId, raceId)
+     dnf int NOT NULL ,
+     fastestLap TIME ,
+     FOREIGN KEY(driverId) REFERENCES Driver(driverId),
+     FOREIGN KEY(raceId) REFERENCES Race(raceId),  
+     CONSTRAINT pk_tbl_RaceResult PRIMARY KEY (driverId, raceId)
 
     );
 
